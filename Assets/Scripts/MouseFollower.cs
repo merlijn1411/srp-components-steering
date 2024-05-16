019d69b45ccc122d47948cd35941cb21b0f20cd7
Berend Weij <berend@studiozoetekauw.nl>
2024-05-16 11:20:43 +0200

    Changed to new version

--- Assets/Scripts/MouseFollower.cs
+++ Assets/Scripts/MouseFollower.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class MouseFollower : MonoBehaviour {
+public class MouseFollower : MonoBehaviour
+{
+    // Dit script wordt nu niet gebruikt in deze repository
+    // maar hier gaan jullie later wel mee aan de slag
 
-    
-    void Update () {
+    void Update()
+    {
         // Als je met de muis klikt: geef dan aan SteeringVehicle door
         // via SetTarget() waar het vliegtuig naartoe moet
+        var targetPosition = GetMousePosition();
+        transform.position = targetPosition;
+    }
+
+    void OnMouseDown()
+    {
+        var targetPosition = GetMousePosition();
+        Debug.Log(targetPosition);
     }
 
     Vector2 GetMousePosition()
--- Assets/Scripts/SteeringVehicle.cs
+++ Assets/Scripts/SteeringVehicle.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
+using UnityEngine.Events;
 
 public class SteeringVehicle : MonoBehaviour
 {
-    [SerializeField] private float _maxSpeed = 20;
+    [SerializeField] private float maxSpeed = 20;
 
     /** hoe zwaarder het object, hoe slechter hij kan bijsturen */
-    [SerializeField] private float _mass = 20;
+    [SerializeField] private float mass = 20;
 
     /** boolean of het object de richting op kijkt waar we naar toe bewegen */
-    [SerializeField] private bool _followPath = false;
+    [SerializeField] private bool followPath = false;
 
-    [SerializeField] private float _arrivalDistance = 0.1f;
+    [SerializeField] private float arrivalDistance = 0.1f;
 
     private Vector2 _currentVelocity;
     private Vector2 _currentPosition;
 
-    /** Vector om de locatie bij te houden waar we heen willen */
-    private Vector2 _currentTarget;
+    public UnityEvent onArrived;
 
-    public Action Arrived;
+    [SerializeField] private Transform targetTransform;
 
     void Start()
     {
@@ -33,58 +32,53 @@ public class SteeringVehicle : MonoBehaviour
     // Elke frametick kijken we hoe we moeten sturen
     void Update()
     {
+        SetTarget(targetTransform.position);
         Seek();
     }
 
     public void SetTarget(Vector2 target)
     {
-        _currentTarget = target;
+        Target = target;
     }
 
-    // van buitenaf kun je de huidige target uitlezen
-    public Vector2 Target
-    {
-        get { return _currentTarget; }
-    }
+    public Vector2 Target { get; private set; }
 
     void Seek()
     {
         // we berekenen eerst de afstand/Vector tot de 'target'		
-        var desiredStep = _currentTarget - _currentPosition;
+        var desiredStep = Target - _currentPosition;
 
         // als een vector ge'normalized' is .. dan houdt hij dezelfde richting
         // maar zijn lengte/magnitude is 1
-        desiredStep.Normalize();
+        var desiredStepNormalized = desiredStep.normalized;
 
         // vermenigvuldigen met de maximale snelheid dan
         // wordt de lengte van deze Vector maxSpeed (aangezien 1 x maxSpeed = maxSpeed)
         // de x en y van deze Vector wordt zo vanzelf omgerekend
-        var desiredVelocity = desiredStep * _maxSpeed;
+        var desiredVelocity = desiredStepNormalized * maxSpeed;
 
         // bereken wat de Vector moet zijn om bij te sturen om bij de desiredVelocity te komen
-        var steeringForce = desiredVelocity - _currentVelocity;
+        var steeringStep = desiredVelocity - _currentVelocity;
 
         // uiteindelijk voegen we de steering force toe maar wel gedeeld door de 'mass'
         // hierdoor gaat hij niet in een rechte lijn naar de target
         // hoe zwaarder het object des te groter de bocht
-        _currentVelocity += steeringForce / _mass;
+        var steeringForce = steeringStep / mass;
+        _currentVelocity += steeringForce;
 
         // Als laatste updaten we de positie door daar onze beweging (velocity) bij op te tellen
         _currentPosition += _currentVelocity * Time.deltaTime;
         transform.position = _currentPosition;
 
-        if (_followPath)
+        if (followPath)
         {
             float angle = Mathf.Atan2(_currentVelocity.y, _currentVelocity.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
 
-        if (Vector2.Distance(_currentTarget, _currentPosition) < _arrivalDistance)
+        if (Vector2.Distance(Target, _currentPosition) < arrivalDistance)
         {
-            if (Arrived != null)
-            {
-                Arrived();
-            }
+            onArrived?.Invoke();
         }
     }
 }
